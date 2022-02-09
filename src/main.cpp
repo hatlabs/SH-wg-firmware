faf059b057248bd8bcdd5926171dffaa4818910b
Matti Airas <mairas@iki.fi>
2022-02-09 14:26:44 +0200

    Migrate the N2K gw example to SensESP

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,173 @@
 // Remove the parts that are not relevant to you, and add your own code
 // for external hardware libraries.
 
-#include "sensesp/sensors/analog_input.h"
-#include "sensesp/sensors/digital_input.h"
-#include "sensesp/sensors/sensor.h"
-#include "sensesp/signalk/signalk_output.h"
-#include "sensesp/system/lambda_consumer.h"
+#include <NMEA2000_CAN.h>
+#include <Seasmart.h>
+
+#include "List.h"
+#include "N2kDataToNMEA0183.h"
 #include "sensesp_app_builder.h"
 
 using namespace sensesp;
+
+constexpr gpio_num_t kCanRxPin = GPIO_NUM_34;
+constexpr gpio_num_t kCanTxPin = GPIO_NUM_32;
+
+const size_t MaxClients = 10;
+bool SendNMEA0183Conversion =
+    true;                  // Do we send NMEA2000 -> NMEA0183 consverion
+bool SendSeaSmart = true;  // Do we send NMEA2000 messages in SeaSmart format
+
+const uint16_t ServerPort =
+    2222;  // Define the port, where served sends data. Use this e.g. on OpenCPN
+
+// Set the information for other bus devices, which messages we support
+const unsigned long TransmitMessages[] PROGMEM = {0};
+const unsigned long ReceiveMessages[] PROGMEM = {/*126992L,*/  // System time
+                                                 127250L,      // Heading
+                                                 127258L,  // Magnetic variation
+                                                 128259UL,  // Boat speed
+                                                 128267UL,  // Depth
+                                                 129025UL,  // Position
+                                                 129026L,   // COG and SOG
+                                                 129029L,   // GNSS
+                                                 130306L,   // Wind
+                                                 0};
+
+uint32_t GetBoardSerialNumber() {
+    uint8_t chipid[6];
+    esp_efuse_mac_get_default(chipid);
+    return chipid[0] + (chipid[1]<<8) + (chipid[2]<<16) + (chipid[3]<<24);
+}
+
+using tWiFiClientPtr = std::shared_ptr<WiFiClient>;
+
+//*****************************************************************************
+void AddClient(WiFiClient &client) {
+  Serial.println("New Client.");
+  clients.push_back(tWiFiClientPtr(new WiFiClient(client)));
+}
+
+//*****************************************************************************
+void StopClient(LinkedList<tWiFiClientPtr>::iterator &it) {
+  Serial.println("Client Disconnected.");
+  (*it)->stop();
+  it = clients.erase(it);
+}
+
+//*****************************************************************************
+void CheckConnections() {
+  WiFiClient client = server.available();  // listen for incoming clients
+
+  if (client) AddClient(client);
+
+  for (auto it = clients.begin(); it != clients.end(); it++) {
+    if ((*it) != NULL) {
+      if (!(*it)->connected()) {
+        StopClient(it);
+      } else {
+        if ((*it)->available()) {
+          char c = (*it)->read();
+          if (c == 0x03) StopClient(it);  // Close connection by ctrl-c
+        }
+      }
+    } else {
+      it = clients.erase(it);  // Should have been erased by StopClient
+    }
+  }
+}
+
+//*****************************************************************************
+void SendBufToClients(const char *buf) {
+  for (auto it = clients.begin(); it != clients.end(); it++) {
+    if ((*it) != NULL && (*it)->connected()) {
+      (*it)->println(buf);
+    }
+  }
+}
+
+#define MAX_NMEA2000_MESSAGE_SEASMART_SIZE 500
+//*****************************************************************************
+// NMEA 2000 message handler
+void HandleNMEA2000Msg(const tN2kMsg &N2kMsg) {
+  if (!SendSeaSmart) return;
+
+  char buf[MAX_NMEA2000_MESSAGE_SEASMART_SIZE];
+  if (N2kToSeasmart(N2kMsg, millis(), buf,
+                    MAX_NMEA2000_MESSAGE_SEASMART_SIZE) == 0)
+    return;
+  Serial.println(buf);
+  SendBufToClients(buf);
+}
+
+#define MAX_NMEA0183_MESSAGE_SIZE 100
+//*****************************************************************************
+void SendNMEA0183Message(const tNMEA0183Msg &NMEA0183Msg) {
+  if (!SendNMEA0183Conversion) return;
+
+  char buf[MAX_NMEA0183_MESSAGE_SIZE];
+  if (!NMEA0183Msg.GetMessage(buf, MAX_NMEA0183_MESSAGE_SIZE)) return;
+  Serial.println(buf);
+  SendBufToClients(buf);
+}
+
+tNMEA2000* nmea2000;
+tN2kDataToNMEA0183* n2k_data_to_nmea0183;
+
+//*****************************************************************************
+void InitNMEA2000() {
+  nmea2000->SetN2kCANMsgBufSize(8);
+  nmea2000->SetN2kCANReceiveFrameBufSize(100);
+  //  nmea2000->SetForwardStream(&Serial);  // PC output on due native port
+  //  nmea2000->SetForwardType(tNMEA2000::fwdt_Text); // Show in clear text
+  // nmea2000->EnableForward(false);                 // Disable all msg
+  // forwarding to USB (=Serial)
+
+  char SnoStr[33];
+  uint32_t SerialNumber = GetBoardSerialNumber();
+  snprintf(SnoStr, 32, "%lu", (long unsigned int)SerialNumber);
+
+  nmea2000->SetProductInformation(
+      SnoStr,                  // Manufacturer's Model serial code
+      130,                     // Manufacturer's product code
+      "N2k->NMEA0183 WiFi",    // Manufacturer's Model ID
+      "1.0.0.1 (2018-04-08)",  // Manufacturer's Software version code
+      "1.0.0.0 (2018-04-08)"   // Manufacturer's Model version
+  );
+  // Det device information
+  nmea2000->SetDeviceInformation(
+      SerialNumber,  // Unique number. Use e.g. Serial number.
+      130,           // Device function=PC Gateway. See codes on
+            // http://www.nmea.org/Assets/20120726%20nmea%202000%20class%20%26%20function%20codes%20v%202.00.pdf
+      25,  // Device class=Inter/Intranetwork Device. See codes on
+           // http://www.nmea.org/Assets/20120726%20nmea%202000%20class%20%26%20function%20codes%20v%202.00.pdf
+      2046  // Just choosen free from code list on
+            // http://www.nmea.org/Assets/20121020%20nmea%202000%20registration%20list.pdf
+  );
+
+  nmea2000->SetForwardType(
+      tNMEA2000::fwdt_Text);  // Show in clear text. Leave uncommented for
+                              // default Actisense format.
+  nmea2000->SetMode(tNMEA2000::N2km_ListenAndNode, 32);
+  // nmea2000->EnableForward(false);
+
+  nmea2000->ExtendTransmitMessages(TransmitMessages);
+  nmea2000->ExtendReceiveMessages(ReceiveMessages);
+  nmea2000->AttachMsgHandler(
+      n2k_data_to_nmea0183);  // NMEA 2000 -> NMEA 0183 conversion
+  nmea2000->SetMsgHandler(
+      HandleNMEA2000Msg);  // Also send all NMEA2000 messages in SeaSmart format
+
+  n2k_data_to_nmea0183->SetSendNMEA0183MessageCallback(SendNMEA0183Message);
+
+  nmea2000->Open();
+}
+
+
+WiFiServer server(ServerPort, MaxClients);
+
+using tWiFiClientPtr = std::shared_ptr<WiFiClient>;
+LinkedList<tWiFiClientPtr> clients;
 
 reactesp::ReactESP app;
 
@@ -25,102 +184,30 @@ void setup() {
   SetupSerialDebug(115200);
 #endif
 
-  // Construct the global SensESPApp() object
-  SensESPAppBuilder builder;
-  sensesp_app = (&builder)
-                    // Set a custom hostname for the app.
-                    ->set_hostname("my-sensesp-project")
-                    // Optionally, hard-code the WiFi and Signal K server
-                    // settings. This is normally not needed.
-                    //->set_wifi("My WiFi SSID", "my_wifi_password")
-                    //->set_sk_server("192.168.10.3", 80)
-                    ->get_app();
+  // Initialize the NMEA2000 library
+  nmea2000 = new tNMEA2000_esp32(kCanTxPin, kCanRxPin);
+  n2k_data_to_nmea0183 = new tN2kDataToNMEA0183(nmea2000, 0);
 
-  // GPIO number to use for the analog input
-  const uint8_t kAnalogInputPin = 36;
-  // Define how often (in milliseconds) new samples are acquired
-  const unsigned int kAnalogInputReadInterval = 500;
-  // Define the produced value at the maximum input voltage (3.3V).
-  // A value of 3.3 gives output equal to the input voltage.
-  const float kAnalogInputScale = 3.3;
+  InitNMEA2000();
 
-  // Create a new Analog Input Sensor that reads an analog input pin
-  // periodically.
-  auto* analog_input = new AnalogInput(
-      kAnalogInputPin, kAnalogInputReadInterval, "", kAnalogInputScale);
-
-  // Add an observer that prints out the current value of the analog input
-  // every time it changes.
-  analog_input->attach([analog_input]() {
-    debugD("Analog input value: %f", analog_input->get());
+  // Handle incoming connections
+  app.onRepeat(1, []() {
+    CheckConnections();
   });
 
-  // Set GPIO pin 15 to output and toggle it every 650 ms
-
-  const uint8_t kDigitalOutputPin = 15;
-  const unsigned int kDigitalOutputInterval = 650;
-  pinMode(kDigitalOutputPin, OUTPUT);
-  app.onRepeat(kDigitalOutputInterval, [kDigitalOutputPin]() {
-    digitalWrite(kDigitalOutputPin, !digitalRead(kDigitalOutputPin));
+  // Handle incoming NMEA 2000 messages
+  app.onRepeat(1, []() {
+    nmea2000->ParseMessages();
+    n2k_data_to_nmea0183->Update();
   });
 
-  // Read GPIO 14 every time it changes
+  //app.onAvailable(Serial, []() {
+  //  // Flush the incoming serial buffer
+  //  while (Serial.available()) {
+  //    Serial.read();
+  //  }
+  //});
 
-  const uint8_t kDigitalInput1Pin = 14;
-  auto* digital_input1 =
-      new DigitalInputChange(kDigitalInput1Pin, INPUT_PULLUP, CHANGE);
-
-  // Connect the digital input to a lambda consumer that prints out the
-  // value every time it changes.
-
-  // Test this yourself by connecting pin 15 to pin 14 with a jumper wire and
-  // see if the value changes!
-
-  digital_input1->connect_to(new LambdaConsumer<bool>(
-      [](bool input) { debugD("Digital input value changed: %d", input); }));
-
-  // Create another digital input, this time with RepeatSensor. This approach
-  // can be used to connect external sensor library to SensESP!
-
-  const uint8_t kDigitalInput2Pin = 13;
-  const unsigned int kDigitalInput2Interval = 1000;
-
-  // Configure the pin. Replace this with your custom library initialization
-  // code!
-  pinMode(kDigitalInput2Pin, INPUT_PULLUP);
-
-  // Define a new RepeatSensor that reads the pin every 100 ms.
-  // Replace the lambda function internals with the input routine of your custom
-  // library.
-
-  // Again, test this yourself by connecting pin 15 to pin 13 with a jumper
-  // wire and see if the value changes!
-
-  auto* digital_input2 = new RepeatSensor<bool>(
-      kDigitalInput2Interval,
-      [kDigitalInput2Pin]() { return digitalRead(kDigitalInput2Pin); });
-
-  // Connect the analog input to Signal K output. This will publish the
-  // analog input value to the Signal K server every time it changes.
-  analog_input->connect_to(new SKOutputFloat(
-      "sensors.analog_input.voltage",         // Signal K path
-      "/sensors/analog_input/voltage",        // configuration path, used in the
-                                              // web UI and for storing the
-                                              // configuration
-      new SKMetadata("V",                     // Define output units
-                     "Analog input voltage")  // Value description
-      ));
-
-  // Connect digital input 2 to Signal K output.
-  digital_input2->connect_to(new SKOutputBool(
-      "sensors.digital_input2.value",          // Signal K path
-      "/sensors/digital_input2/value",         // configuration path
-      new SKMetadata("",                       // No units for boolean values
-                     "Digital input 2 value")  // Value description
-      ));
-
-  // Start networking, SK server connections and other SensESP internals
-  sensesp_app->start();
 }
 
 void loop() { app.tick(); }
