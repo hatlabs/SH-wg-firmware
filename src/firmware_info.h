90e22f6fc58d8149208bed07e26e73a057ddfa53
Matti Airas <mairas@iki.fi>
2022-03-15 16:37:38 +0200

    Bump version: 0.4.0 → 0.4.1-alpha.1

--- src/firmware_info.h
+++ src/firmware_info.h
@@ -7,7 +7,7 @@
 constexpr char const * kFirmwareName = "SH-wg/dev";
 
 /** Firmware version string*/
-constexpr char const * kFirmwareVersion = "0.4.0";
+constexpr char const * kFirmwareVersion = "0.4.1-alpha.1";
 
 /** Firmware version encoded in an easily sortable uint32_t value */
 constexpr uint32_t kFirmwareHexVersion = 0x000400ff;
