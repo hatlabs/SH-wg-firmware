0826737046c99c2f39c64ce8ffdff20c8b984dd3
Matti Airas <mairas@iki.fi>
2022-04-21 15:39:29 +0300

    Implement YDWGRawToCANFrameTransform

--- src/ydwg_raw_parser.h
+++ src/ydwg_raw_parser.h
@@ -3,9 +3,30 @@
 
 #include <Arduino.h>
 #include <N2kMsg.h>
+#include <sys/time.h>
 
 #include "can_frame.h"
+#include "sensesp/transforms/transform.h"
 
-bool YDWGRawToCANFrame(CANFrame& frame, const String& ydwg_raw, struct timeval& timestamp);
+using namespace sensesp;
+
+bool YDWGRawToCANFrame(CANFrame& frame, char& direction,
+                       struct timeval& timestamp, const String& ydwg_raw);
+
+class YDWGRawToCANFrameTransform : public Transform<String, CANFrame> {
+ public:
+  YDWGRawToCANFrameTransform() : Transform<String, CANFrame>() {}
+
+  void set_input(const String ydwg_raw_str, uint8_t input_channel) override {
+    CANFrame frame;
+    char direction;
+    struct timeval timestamp;
+    if (YDWGRawToCANFrame(frame, direction, timestamp, ydwg_raw_str)) {
+      emit(frame);
+    } else {
+      debugW("YDWG RAW string parsing failed: %s", ydwg_raw_str.c_str());
+    }
+  }
+};
 
 #endif  // SH_WG_FIRMWARE_YDWG_RAW_PARSER_H_
