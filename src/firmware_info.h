8724f1dcaf7f45a9137c1d5a84717026c22ae5ce
Matti Airas <mairas@iki.fi>
2022-08-23 15:21:44 +0300

    Bump version: 0.6.1 → 0.7.0

--- src/firmware_info.h
+++ src/firmware_info.h
@@ -7,7 +7,7 @@
 constexpr char const * kFirmwareName = "SH-wg/dev";
 
 /** Firmware version string*/
-constexpr char const * kFirmwareVersion = "0.6.1";
+constexpr char const * kFirmwareVersion = "0.7.0";
 
 /** Firmware version encoded in an easily sortable uint32_t value */
 constexpr uint32_t kFirmwareHexVersion = 0x000601ff;
